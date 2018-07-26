31d4b8b0ea7691d2fdf88c1549cfc094f0352b14
ruffensteint <hyde.kris@gmail.com>
2018-07-26 00:34:36 -0400

    OVERHAUL

---
+++ home/js/gifhover.js
@@ -0,0 +1,15 @@
+$(document).ready(function()
+{
+	"use strict";
+    $(".gif").hover(
+        function()
+        {
+          var src = $(this).attr("src");
+          $(this).attr("src", src.replace(/\.png$/i, ".gif"));
+        },
+        function()
+        {
+          var src = $(this).attr("src");
+          $(this).attr("src", src.replace(/\.gif$/i, ".png"));
+        });
+});
